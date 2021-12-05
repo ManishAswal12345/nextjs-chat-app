2f49a6eac0b802feb91b8b14e1e1a5a72cce978d
ManishAswal12345 <manish.aswal12345@gmail.com>
2021-12-05 23:24:26 +0530

    Working Chat App

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,57 @@
-import React from "react";
+import React, {useContext} from "react";
+
+import {Context} from "../context";
+
+import { useRouter } from "next/router";
+
+import axios from "axios";
+
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const { username, secret, setUsername, setSecret } = useContext(Context);
+
+  const router = useRouter()
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    if(username.length === 0 || secret.length === 0) return;
+
+    axios
+      .put(
+        "https://api.chatengine.io/users/",
+        { username, secret },
+        { headers: { "Private-key": "c1cef38e-f3a5-4ebf-9884-49a61d409adc" } }
+      )
+      .then((r) => router.push("/chats"));
+  }
+
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
+          <div className="auth-title">NextJS Chat</div>
+          <div className="input-character">
+            <input
+              placeholder="Email/Username"
+              className="text-input"
+              onChange={e => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-input"
+              onChange={e => setSecret(e.target.value)}
+            />
+            <button type="submit" className="submit-button">
+              Login / Sign Up
+            </button>
+          </div>
+
+        </form>
+
+      </div>
+    </div>
+    
+  )
 }
